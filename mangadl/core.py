e55badbd93c9cbe434a643e9c29547b1cee8512c
alethiophile <mail@alethiophile.dev>
2022-08-09 12:38:58 -0600

    More changes
    Rewrote Batoto backend with new backend protocol (and for new Batoto website),
    implemented backend lookup, added queue limitation on downloads, added tracking
    of already-downloaded chapters

--- mangadl/core.py
+++ mangadl/core.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 
 from .trashscanlations import TrashScanlations
-from mangadl.util import to_filename, sessionget
-import click, trio, asks, urllib.parse, os, zipfile
+from .batoto import Batoto
+from .util import to_filename, sessionget
+import click, trio, asks, urllib.parse, os, zipfile, re, qtoml
+
+backend_res = [
+    ( 'trashscanlations',
+      re.compile("^https://trashscanlations.com/")),
+    ( 'batoto',
+      re.compile("^https://bato.to/")),
+]
+
+backend_objs = {
+    'trashscanlations': TrashScanlations,
+    'batoto': Batoto,
+}
+
+def find_backend(url):
+    for name, pattern in backend_res:
+        if pattern.match(url):
+            return name
+    raise ValueError(f"Couldn't find backend for URL '{url}'")
 
 async def cbz_writer(recv_channel, fn):
     recvd = {}
@@ -19,7 +38,7 @@ async def cbz_writer(recv_channel, fn):
                 written += 1
         zipf.close()
 
-async def img_fetcher(send_channel, ind, img_url):
+async def img_fetcher(dls, send_channel, ind, img_url):
     r = await sessionget(img_url)
     img = r.content
     parts = urllib.parse.urlparse(img_url)
@@ -27,29 +46,85 @@ async def img_fetcher(send_channel, ind, img_url):
         ts = (ind, os.path.basename(parts.path), img)
         print(f"fetch #{ind} {img_url}")
         await send_channel.send(ts)
+        dls.release_on_behalf_of(ind)
 
-async def fetch_chapter(title_fn, ind, cle, il):
-    cbz_fn = f"{title_fn}.{ind:03}.{to_filename(cle[0])}.cbz"
+async def fetch_chapter(title_fn, cbz_fn, cle, il):
     async with trio.open_nursery() as nursery:
         send_channel, recv_channel = trio.open_memory_channel(3)
+        # limit total downloads, to keep from having to cache too much before
+        # writing to disk
+        dls = trio.CapacityLimiter(10)
         async with send_channel, recv_channel:
             nursery.start_soon(cbz_writer, recv_channel.clone(), cbz_fn)
             for n, i in enumerate(il):
-                nursery.start_soon(img_fetcher, send_channel.clone(),
+                await dls.acquire_on_behalf_of(n)
+                nursery.start_soon(img_fetcher, dls, send_channel.clone(),
                                    n, i)
 
-async def trio_main(url):
-    f = TrashScanlations()
-    title, cl = await f.get_chapters(url)
-    title_fn = to_filename(title)
+async def trio_main(url, num_chapters, dry_run):
+    update_file = False
+    fetched_data = {}
+    if os.path.exists(url):
+        update_file = True
+        with open(url) as inp:
+            fetched_data = qtoml.load(inp)
+
+    real_url = (fetched_data['url'] if update_file else url)
+    print(f"Fetching URL {real_url}")
+
+    backend_name = (fetched_data['backend'] if update_file else
+                    find_backend(url))
+    handler = backend_objs[backend_name]()
+
+    title, cl = await handler.get_chapters(real_url)
+    title_fn = (fetched_data['title_fn'] if update_file else
+                to_filename(title))
+
+    update_fn = (url if update_file else f"{title_fn}.toml")
+
+    if os.path.exists(update_fn):
+        update_file = True
+        with open(update_fn) as inp:
+            fetched_data = qtoml.load(inp)
+
+    if not update_file:
+        fetched_data = { 'title': title, 'title_fn': title_fn, 'url': url,
+                         'backend': backend_name, 'chapter_urls': {} }
+
+    if dry_run:
+        print(title)
+        print(cl)
+        il = await handler.get_pages(cl[0][1])
+        print(il)
+        return
+
+    n_fetched = 0
     for n, c in enumerate(cl):
+        if c[1] in fetched_data['chapter_urls']:
+            if os.path.exists(fetched_data['chapter_urls'][c[1]]):
+                print(f"Already have chapter {c[0]}")
+                continue
+            elif fetched_data['chapter_urls'][c[1]] == 'SKIP':
+                print(f"Chapter {c[0]} marked for skipping")
+                continue
+        cbz_fn = f"{title_fn}.{n:03}.{to_filename(c[0])}.cbz"
         print(f"Fetching chapter {c[0]}")
-        il = await f.get_pages(c[1])
-        await fetch_chapter(title_fn, n, c, il)
+        il = await handler.get_pages(c[1])
+        await fetch_chapter(title_fn, cbz_fn, c, il)
+        fetched_data['chapter_urls'][c[1]] = cbz_fn
+        n_fetched += 1
+        if num_chapters is not None and n_fetched >= num_chapters:
+            break
+
+    with open(update_fn, 'w') as out:
+        qtoml.dump(fetched_data, out)
 
 @click.command()
+@click.option('--num-chapters', '-n', type=int,
+              help="Number of chapters to fetch (default all)")
+@click.option("--dry-run", type=bool, is_flag=True,
+              help="Don't download, just test backend")
 @click.argument('url', type=str)
-def main(url):
+def main(url, num_chapters, dry_run):
     asks.init('trio')
-    print(f"Fetching URL {url}")
-    trio.run(trio_main, url)
+    trio.run(trio_main, url, num_chapters, dry_run)
